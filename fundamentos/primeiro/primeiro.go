e94e470d1d71404e03655e6ce65f808e0bfaa394
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2017-12-07 13:00:42 -0200

    finalizado comandos basico

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -1,5 +1,10 @@
+// Programas executaveis iniciam pelo pacote main
 package main
 
+/*
+Os codigo em Go sao organizados em pacotes
+e para usalos é necessario declarar um ou varios pacotes
+*/
 import "fmt"
 
 func main() {
