dcc280e16f574554ba33b4d52f473541b29dbf47
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-12-08 13:40:33 -0200

    aff dos print

---
+++ fundamentos/prints/prints.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Mesmo")
+	fmt.Print(" linha")
+
+	fmt.Printf("Nova Linha")
+
+	x := 32.32432
+
+	xs := fmt.Sprint(x)
+	fmt.Println("O valor de x é " + xs)
+
+	fmt.Printf("O valor de x é %.2f", x)
+
+	a := 1
+	b := 2.9894
+	c := false
+	d := "ronaldo"
+	fmt.Printf("\n", d)
+
+}
+
