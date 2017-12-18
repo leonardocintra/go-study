852c1dd75a6460c150372307f20011c9a00c7425
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2017-12-18 13:17:33 -0200

    ponteiro

---
+++ funcoes/ponteiro/ponteiro.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func inc1(n int) {
+	n++
+}
+
+// revisao: um ponteiro é representado por um *
+func inc2(n *int) {
+	// revisao: * é usado para desreferenciar, ou seja
+	// ter acesso ao valor no qual o ponteiro aponta
+	*n++
+}
+
+func main() {
+	n := 1
+	inc1(n) // por valor
+	fmt.Println(n)
+
+	// revisao o & é usado para obter o endereco da variavel
+	inc2(&n) // por referencia
+	fmt.Println(n)
+}
