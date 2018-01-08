69876457d48c5629aea4ba1ff2901f965ba6cb6f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-01-08 13:11:40 -0200

    channel1 e channel2

--- concorrencia/channel1/channel.go
+++ concorrencia/channel1/channel.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	
+	ch := make(chan int, 1)
+	ch <- 1 // enviando dados para o canal (escrita)
+
+	<-ch // recebendo dados do canal (leitura)
+
+	ch <- 2
+	fmt.Println(<-ch)
+
 }
