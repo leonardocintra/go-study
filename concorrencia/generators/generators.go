9a44f11e168542b8ac2e556f4e76b69e3b607237
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-01-10 13:35:15 -0200

    da hora os generators

---
+++ concorrencia/generators/generators.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// No Google I/O 2012 - GO Concurrency Patterns
+
+// <-chan - canal somente-leitura
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+
+		// funcao anonima
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)</title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return c
+}
+
+func main() {
+	t1 := titulo("https://www.cod3r.com.br", "https://www.google.com")
+	t2 := titulo("https://amazon.com", "https://www.youtube.com")
+
+	fmt.Println("Primeiros", <-t1, "|", <-t2)
+	fmt.Println("Segundos", <-t1, "|", <-t2)
+}
