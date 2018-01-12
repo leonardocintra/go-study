37a90503afdc828ab13c2849480d45890d084a4e
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-01-12 13:06:00 -0200

    multiplexar

---
+++ concorrencia/multiplexar/multiplexar.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/leonardocintra/html"
+)
+
+func encaminhar(origem <-chan string, destino chan string) {
+	for {
+		destino <- <-origem
+	}
+}
+
+// Seccao 9 aula 77
+// multiplexar - misturar (mensagens) num canal
+
+func juntar(entrada1, entrada2 <-chan string) <-chan string {
+	c := make(chan string)
+	go encaminhar(entrada1, c)
+	go encaminhar(entrada2, c)
+	return c
+}
+
+func main() {
+	c := juntar(
+		html.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
+		html.Titulo("https://amazon.com", "https://www.youtube.com"),
+	)
+
+	fmt.Println(<-c, "|", <-c)
+	fmt.Println(<-c, "|", <-c)
+}
