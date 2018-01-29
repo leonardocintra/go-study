5dc5b2ad79fe124e9ad3c27126b668ccb6bf05ba
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-01-29 12:57:26 -0200

    sql e insert sql

---
+++ sql/insert/insert.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/cursogo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stmt, _ := db.Prepare("insert into usuarios(nome) values(?)")
+	stmt.Exec("Maria")
+	stmt.Exec("João Paulo")
+
+	res, _ := stmt.Exec("Pedro")
+	id, _ := res.LastInsertId()
+	fmt.Println(id)
+
+	linhas, err := res.RowsAffected()
+	fmt.Println(linhas)
+}
